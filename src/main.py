584b0b2ee849ec6f47a21898600e4a69609f96cd
Daria Omelkina <odasha2001@gmail.com>
2021-02-19 04:00:00 +0200

    Started adding DFA convertion.

--- src/main.py
+++ src/main.py
@@ -27,10 +27,11 @@ def parse(input_file_path):
                         element_transitions[element].append((x, y))
                         state_transitions[x].append((element, y))
 
-    return [index_of_initial_state, list_of_indices_of_terminal_states, element_transitions, state_transitions]
+    return [index_of_initial_state, list_of_indices_of_terminal_states,
+            element_transitions, state_transitions]
 
 
-def get_normal_q_representation(transitions):
+def get_normal_delta_representation(transitions):
     """
     :param transitions: {state:[(element1,state1), (element1, state2), ...]}
     :return: {state:{element:(state1, state2, ...), ...}
@@ -47,25 +48,32 @@ def get_normal_q_representation(transitions):
 
 
 def epsilon_nfa_to_nfa(fa):
+    # TODO: add list of initial states
     # First, we will change index of initial state to a list of indexes, so algorithm can work
-    fa[0] = [fa[0]]
+    # fa[0] = [fa[0]]
 
     if '\\e' in fa[2]:
         for transition in fa[2]['\\e']:
             start_state = transition[0]
             end_state = transition[1]
 
-            if start_state in fa[0]:
-                fa[0].append(end_state)
+            # if start_state in fa[0]:
+            #     fa[0].append(end_state)
 
             if end_state in fa[1]:
                 fa[1].append(start_state)
 
             for end_transition in fa[3][end_state]:
-                fa[3][start_state].append(end_transition)
+                if '\\e' not in end_transition:
+                    fa[3][start_state].append(end_transition)
                 fa[2][end_transition[0]].append((start_state, end_transition[1]))
 
-        del automata[2]['\\e']
+        del fa[2]['\\e']
+
+    for element in fa[3]:
+        for transition in fa[3][element]:
+            if '\\e' in transition:
+                fa[3][element].remove(transition)
     return fa
 
 
@@ -76,18 +84,43 @@ if __name__ == '__main__':
     # Parsing file to get automata represented as a list:
     # [index of initial state, list of indices of terminal states,
     #  transitions: element:[(pair of states), ....]
-    #  state transitions: ]
+    #  state transitions: {state:[(element1,state1), (element1, state2), ...]} ]
     automata = parse(given_file_path)
 
     # If we have epsilon-NFA, convert it to NFA:
     automata = epsilon_nfa_to_nfa(automata)
 
     # Convert automata to DFA:
-    # Initially new Q is empty
+    # Initially new Q is empty, I suppose
     new_element_transitions = dict()
     new_state_transitions = dict()
+    q = []
+    new_delta = dict()
 
     # костильний метод для отримання переходів,
     # приберу його коли буду нормально парсити, а не як зараз:/
-    q = get_normal_q_representation(automata[3])
-    print(q)
+    delta = get_normal_delta_representation(automata[3])
+    # for i in delta.items():
+    #     print(i)
+
+    q.append((automata[0],))
+
+    for state in q:
+        new_delta[state] = dict()
+
+        for element in state:
+            for transition in delta[element]:
+                transition_state_set = delta[element][transition]
+                if transition not in new_delta[state]:
+                    new_delta[state][transition] = transition_state_set
+                else:
+                    new_delta[state][transition].union(transition_state_set)
+                new_state = tuple(transition_state_set)
+                # for other_state in q:
+                #     if element in state:
+                #         new_delta[state][transition].union(new_delta[other_state][element])
+                if new_state not in q:
+                    q.append(new_state)
+    print(f"     {'  '.join(list(str(i) for i in range(len(automata[2]))))}")
+    for i in new_delta:
+        print(f"{i}   {'  '.join(list(str(i) for i in range(len(new_delta))))}")
