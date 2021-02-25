0d002e09341ab1ac4e89cf36462d3dba48ac3e55
Daria Omelkina <odasha2001@gmail.com>
2021-02-26 00:47:31 +0200

    Fixed parsing, cleaned code. First iteration finished.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NFA to cDFA converter
 Task for the TCSC@UCU.
+
+## Usage:
+Example:
+```bash
+python3 src/main.py input_files/input1.txt
+```
+Results will be displayed in the terminal.
+(saving results in txt file -– coming soon(er or later))
--- src/main.py
+++ src/main.py
@@ -1,55 +1,43 @@
 import sys
 
 
-# TODO: implement adequate parsing, not a parody that I have now
+# TODO: add terminal states checking and converting
+
 def parse(input_file_path):
-    element_transitions = dict()  # element: (start_state, end_state)
-    state_transitions = dict()  # start_state: (element, end_state)
     with open(input_file_path, "r") as input_file:
         size_of_q, size_of_sigma = [int(size) for size in input_file.readline().split()]
 
         index_of_initial_state = int(input_file.readline().split()[0])
-        list_of_indices_of_terminal_states = \
-            [int(term) for term in list(input_file.readline().split())[1:]]
+        set_of_indices_of_terminal_states = \
+            set(int(term) for term in list(input_file.readline().split())[1:])
 
+        transitions = dict()
+        for i in range(size_of_q):
+            transitions[i] = {}
+
+        set_of_elements = set()
         for line in range(size_of_sigma + 1):
             element = input_file.readline().split()[0]
-            element_transitions[element] = []
+            set_of_elements.add(element)
 
             for x in range(size_of_q):
                 row = input_file.readline().split()
-
-                if x not in state_transitions:
-                    state_transitions[x] = []
-
                 for y in range(size_of_q):
                     if row[y] == "1":
-                        element_transitions[element].append((x, y))
-                        state_transitions[x].append((element, y))
+                        if element not in transitions[x]:
+                            transitions[x][element] = set()
+                        transitions[x][element].add(y)
 
-    return [index_of_initial_state, list_of_indices_of_terminal_states,
-            element_transitions, state_transitions]
-
-
-def get_normal_delta_representation(transitions):
-    """
-    :param transitions: {state:[(element1,state1), (element1, state2), ...]}
-    :return: {state:{element:(state1, state2, ...), ...}
-    """
-    adequate_q = dict()
-    for state in transitions:
-        adequate_q[state] = dict()
-        for transition in transitions[state]:
-            if transition[0] in adequate_q[state]:  # element
-                adequate_q[state][transition[0]].add(transition[1])
-            else:
-                adequate_q[state][transition[0]] = {transition[1]}
-    return adequate_q
+    return {"initial": {index_of_initial_state},
+            "terminal": set_of_indices_of_terminal_states,
+            "transitions": transitions,
+            "elements": set_of_elements}
 
 
-def epsilon_nfa_to_nfa(prev):
-    # TODO: add term states
+def epsilon_nfa_to_nfa(automata):
+    prev = automata["transitions"]
     flag = 1
+    new = {}
     while flag:
         new = {}
         flag = 0
@@ -65,109 +53,14 @@ def epsilon_nfa_to_nfa(prev):
                 else:
                     new[state][transition[0]].add(transition[1])
         prev = new
-    return new
+    automata["transitions"] = new
+    return automata
 
 
-def merge(dict1, dict2):
-    for transition in dict2.items():
-        if transition[0] in dict1:
-            dict1[transition[0]] | set(transition[1])
-        else:
-            dict1[transition[0]] = transition[1]
-    return dict1
-
-
-def find_entries(state, dct):
-    entries = set()
-    for key in dct:
-        if set(state) & set(key):
-            entries.add(key)
-    return entries
-
-
-def prepare_for_dfa(dct):
-    new_dct = {}
-    for i in dct.items():
-        new_dct[(i[0],)] = i[1]
-    return new_dct
-
-
-if __name__ == '__main__':
-    # # TODO: add checking of the argument provided
-    # given_file_path = sys.argv[1]
-    #
-    # # Parsing file to get automata represented as a list:
-    # # [index of initial state, list of indices of terminal states,
-    # #  transitions: element:[(pair of states), ....]
-    # #  state transitions: {state:[(element1,state1), (element1, state2), ...]} ]
-    # automata = parse(given_file_path)
-    #
-    # # If we have epsilon-NFA, convert it to NFA:
-    # automata = epsilon_nfa_to_nfa(automata)
-    #
-    # # Convert automata to DFA:
-    # # Initially new Q is empty, I suppose
-    # new_element_transitions = dict()
-    # new_state_transitions = dict()
-    # q = []
-    # new_delta = dict()
-    #
-    # # костильний метод для отримання переходів,
-    # # приберу його коли буду нормально парсити, а не як зараз:/
-    # delta = get_normal_delta_representation(automata[3])
-    # # for i in delta.items():
-    # #     print(i)
-    #
-    # q.append((automata[0],))
-    #
-    # for state in q:
-    #     new_delta[state] = dict()
-    #
-    #     for element in state:
-    #         for transition in delta[element]:
-    #             transition_state_set = delta[element][transition]
-    #             if transition not in new_delta[state]:
-    #                 new_delta[state][transition] = transition_state_set
-    #             else:
-    #                 new_delta[state][transition].union(transition_state_set)
-    #             new_state = tuple(transition_state_set)
-    #             # for other_state in q:
-    #             #     if element in state:
-    #             #         new_delta[state][transition].union(new_delta[other_state][element])
-    #             if new_state not in q:
-    #                 q.append(new_state)
-    # print(f"     {'  '.join(list(str(i) for i in range(len(automata[2]))))}")
-    # for i in new_delta:
-    #     print(f"{i}   {'  '.join(list(str(i) for i in range(len(new_delta))))}")
-    prev = {0: {'\\e': {1}},
-            1: {'\\0': {0, 1}, '\\1': {2}},
-            2: {'\\e': {0}}}
-    init_states = {0, 1, 2}
-    init_elements = {'\\0', '\\1'}
-    initial = {0}
-    term = {2}
-
-    # Epsilon-NFA to NFA:
+def nfa_to_dfa(automata):
+    prev = prepare_for_dfa(automata["transitions"])
     flag = 1
-    while flag:
-        new = {}
-        flag = 0
-        for state in prev:
-            new[state] = {}
-            for transition in prev[state].items():
-                if transition[0] == '\\e':
-                    for new_state in transition[1]:
-                        new[state] = merge(new[state], prev[new_state])
-                    flag = 1
-                elif transition[0] not in new[state]:
-                    new[state][transition[0]] = transition[1]
-                else:
-                    new[state][transition[0]].add(transition[1])
-        prev = new
-
-    # NFA to DFA:
-    prev = prepare_for_dfa(prev)
-    flag = 1
+    new = {}
     while flag:
         new = {}
         flag = 0
@@ -202,19 +95,67 @@ if __name__ == '__main__':
                             del new[tuple(key)]
 
         prev = new
+    automata["transitions"] = new
+    return automata
 
-    # DFA to cDFA:
+
+def dfa_to_cdfa(automata):
     absorbing_state = "absorbing"
     flag = 0
-    for state in new:
-        for element in init_elements:
-            if element not in new[state]:
+    for state in automata["transitions"]:
+        for element in automata["elements"] - {'\\e'}:
+            if element not in automata["transitions"][state]:
                 flag = 1
-                new[state][element] = {absorbing_state}
+                automata["transitions"][state][element] = {absorbing_state}
     if flag:
-        new[(absorbing_state,)] = {}
-        for element in init_elements:
-            new[(absorbing_state,)][element] = {absorbing_state}
+        automata["transitions"][(absorbing_state,)] = {}
+        for element in automata["elements"] - {'\\e'}:
+            automata["transitions"][(absorbing_state,)][element] = {absorbing_state}
+    return automata
 
-    for i in new:
-        print(f"{i}   {new[i]}")
+
+def merge(dict1, dict2):
+    for transition in dict2.items():
+        if transition[0] in dict1:
+            dict1[transition[0]] | set(transition[1])
+        else:
+            dict1[transition[0]] = transition[1]
+    return dict1
+
+
+def find_entries(state, dct):
+    entries = set()
+    for key in dct:
+        if set(state) & set(key):
+            entries.add(key)
+    return entries
+
+
+def prepare_for_dfa(dct):
+    new_dct = {}
+    for i in dct.items():
+        new_dct[(i[0],)] = i[1]
+    return new_dct
+
+
+if __name__ == '__main__':
+    # TODO: add checking of the argument provided
+    given_file_path = sys.argv[1]
+
+    # Parsing file to get automata represented as a list:
+    automata = parse(given_file_path)
+
+    # Epsilon-NFA to NFA:
+    automata = epsilon_nfa_to_nfa(automata)
+
+    # NFA to DFA:
+    automata = nfa_to_dfa(automata)
+
+    # DFA to cDFA:
+    automata = dfa_to_cdfa(automata)
+
+    # Results:
+    # TODO: write results to a file
+
+    for i in automata["transitions"]:
+        print(f"{i}    " + str(automata["transitions"][i]))
